83a763bf17b640b35e713b27879ec643be9b4c3f
James L Bergeron <jberg475@mtroyal.ca>
2017-03-12 22:00:11 -0600

    Basic Listener Functionality

---
+++ NodeJsDemo/app.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+"use strict";
+var express = require('express');
+var server = express();
+server.listen(1234, function () {
+    console.log("Server on port 1234 ready");
+});
